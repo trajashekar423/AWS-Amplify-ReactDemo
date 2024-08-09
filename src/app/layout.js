84197e65efdbd5fe9d0e14f1ca61ec907c2508fa
trajashekar423 <trajashekar423@gmail.com>
2024-08-09 11:42:44 +0530

    navbar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import '@aws-amplify/ui-react/styles.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
